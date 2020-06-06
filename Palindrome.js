50a342e4702a7dbdfff8587813f4716f9528b14a
PranahithBabu <59257365+PranahithBabu@users.noreply.github.com>
2020-06-06 12:37:51 +0530

    Add files via upload

---
+++ Palindrome.js
@@ -0,0 +1,24 @@
+// Initially we will be creating a function named as palindrome & we pass an argument str which is string.
+function palindrome(str) 
+{
+  // Now we take the original value & convert all the characters into lower case.
+  // The 'W' is used for avoiding all the non-alphanumeric characters.
+  var original = str.toLowerCase().replace(/[\W_]+/g,"");
+  // Now we are telling the function to read from backside.
+  var backwards = original.split("").reverse().join("");
+  // Now we have to check whether the original & backward text is similar or not.
+  // 'If' condition checks if they are equal & then returns the true condition. 
+  if (original == backwards) 
+  {
+	return true;
+  } 
+  // 'Else' condition is executed if they are not equal & then returns false condition.
+  else 
+  {
+	return false;
+  }
+}
+// The console.log is used as a print statement.
+console.log(palindrome("race Car"));
+
+// Output :- true
